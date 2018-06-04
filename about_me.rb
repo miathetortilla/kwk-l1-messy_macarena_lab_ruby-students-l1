b368669871fe438d6c81ce60c942457e220f6e83
miathetortilla <mia.lamontagne@gmail.com>
2018-06-04 21:55:44 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,14 @@
+def info 
+  puts "My name is Mia."
+  puts "I am from Aurora, Colorado."
+  puts "I am 17 years old."
+end
+
+def favorites
+  puts "My favorite food is pasta."
+  puts "My favorite color is red."
+  puts "My favorite clothing store is Goodwill."
+end
+
+info
+favorites
